cf59d619dfa42592483c46f5acbaf8d4d4b4d0ae
yillkid <yillkid@gmail.com>
2024-12-22 21:57:50 +0800

    2.py

---
+++ examples/data_connect/2.py
@@ -0,0 +1,33 @@
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.llms import OpenAI
+from langchain.document_loaders import WikipediaLoader
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import Chroma
+
+# 文檔加載器
+query_wiki = "Artificial Intelligence"
+docs = WikipediaLoader(query = query_wiki, lang = "en", load_max_docs = 2).load()
+
+# 分割器
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=100,  # 每個段落的字符數
+    chunk_overlap=20,  # 段落間的重疊字符數
+    length_function=len,
+)
+
+# 分割文本
+split_texts = text_splitter.split_text(docs[0].page_content)
+
+# 列印出分割後的文本段落
+for i, chunk in enumerate(split_texts):
+    print(f"Chunk {i+1}:\n{chunk}\n")
+
+# 計算 Embeddings
+# pip3 install tiktoken
+
+embeddings_model = OpenAIEmbeddings()
+embeddings = embeddings_model.embed_documents(docs[0].page_content)
+
+print(str(len(embeddings)))
+print(str(len(embeddings[0])))
+print(embeddings[0])
